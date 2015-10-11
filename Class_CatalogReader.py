2ef0ec52245e6eb1b7da819f18f64d49c1a1cb20
gcanasherrera <guadalupecah@gmail.com>
2015-10-11 20:55:18 +0200

    changed

--- Class_CatalogReader.py
+++ Class_CatalogReader.py
@@ -44,7 +44,7 @@ class CatalogReader(object):
         
         names = ["number", "flux_iso", "fluxerr_iso", "mag_iso", "magger_iso", "mag_aper_1", "magerr_aper_1", "mag", "magger", "flux_max", "isoarea", "x", "y", "ra", "dec", "ixx", "iyy", "ixy", "ixxWIN", "iyyWIN", "ixyWIN", "A", "B", "theta", "enlogation", "ellipticity", "FWHM", "flags", "class_star"]
         
-        if catalog.endswith(suffix) == False:
+        if self.catalog.endswith(suffix) == False:
             self.transform(self.catalog)
             self.fcat = np.genfromtxt(self.catalog_fiat, names=names)
 
