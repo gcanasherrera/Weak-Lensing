2d5d8dc93cf3316a365c83bb31553a6244de4b5e
gcanasherrera <guadalupecah@gmail.com>
2015-10-18 19:11:40 +0200

    corrected mag

--- WL_Simulation_execute_mag_1.py
+++ WL_Simulation_execute_mag_1.py
@@ -29,7 +29,7 @@ import math
 
 
 #Define arrays for plotting
-mag_input = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+mag_input = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30]
 #mag_input = [7,8,9,10]
 mag_output_sex = []
 mag_output_wayback = []
