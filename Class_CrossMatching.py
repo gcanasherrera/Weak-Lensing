c55961f63851c63619312328dc127de96c506d81
gcanasherrera <guadalupecah@gmail.com>
2015-11-03 19:02:47 +0100

    Cross matching

--- Class_CrossMatching.py
+++ Class_CrossMatching.py
@@ -39,7 +39,6 @@ class ObjectCreator(object):
     
     """
         Constructor that defines attribute of future class-objects
-        
     """
     
     def __init__(self, catalog_1, catalog_2):  #define attributes of future class-objects
@@ -58,11 +57,11 @@ class ObjectCreator(object):
         self.posible_obj_index_2 = []
 
 
-    def positions_array_creator():
+    def positions_array_creator(self):
         self.positions_cat_1 = zip(self.x_1.ravel(), self.y_1.ravel())
         self.positions_cat_2 = zip(self.x_2.ravel(), self.y_2.ravel())
 
-    def kdtree(n):
+    def kdtree(self, n=1):
         self.positions_array_creator()
         tree_1 = spatial.KDTree(self.positions_cat_1)
         self.posible_obj_distances_1, self.posible_obj_index_1 = tree_1.query(self.positions_cat_2, distance_upper_bound = n)
@@ -70,44 +69,79 @@ class ObjectCreator(object):
         tree_2 = spatial.KDTree(self.positions_cat_2)
         self.posible_obj_distances_2, self.posible_obj_index_2 = tree_2.query(self.positions_cat_1, distance_upper_bound = n)
 
-    def catalog_writter():
-        fitting_file_ellip= '{}_fit_pol.fcat'.format(FILE_NAME)
-        f_1=open(fitting_file_ellip, 'w')
+    def catalog_writter(self, FILE_NAME, compare = ''):
+        catalog_def= '{}_fit_pol.fcat'.format(FILE_NAME)
+        f_1=open(catalog_def, 'w')
         f_1.write('# fiat 1.0\n')
-        f_1.write('# nobj_init = {} / before clipping \n'.format(longitud))
-        f_1.write('# nob_fin = {} / after clipping\n'.format(len(x_int_1)))
-        f_1.write('# npar = 15 / number of parameters \n')
-        f_1.write('# order = 4 / order of polynomial fit \n')
-        f_1.write('# nfits = 2 / e_1, e_2 \n')
-        f_1.write('# nsig = 3.0 / number of sigma for clipping \n')
-        f_1.write('# ttype1 = yorder0 \n')
-        f_1.write('# ttype2 = yorder1 \n')
-        f_1.write('# ttype3 = yorder2 \n')
-        f_1.write('# ttype4 = yorder3 \n')
-        f_1.write('# ttype5 = yorder4 \n')
-        f_1.write('# e_1 / e_1=e*cos(2*theta)\n')
-        f_1.write('# residual_mean = {} \n'. format(np.mean(residual_1, dtype=np.float64)))
-        f_1.write('# residual_mean = {} \n'. format(np.std(residual_1, dtype=np.float64)))
+        f_1.write('# written by Class_CrossMatching.py\n')
+        f_1.write('# TTYPE1 = NUMBER / Running object number \n')
+        f_1.write('# TTYPE2 = FLUX_ISO / Isophotal flux \n')
+        f_1.write('# TTYPE3 = FLUXERR_ISO / RMS error for isophotal flux   \n')
+        f_1.write('# TTYPE4 = MAG_ISO / Isophotal magnitude    \n')
+        f_1.write('# TTYPE5 = MAGERR_ISO / RMS error for isophotal magnitude   \n')
+        f_1.write('# TTYPE6 = MAG_APER_1 / Fixed aperture magnitude       \n')
+        f_1.write('# TTYPE7 = MAGERR_APER_1 / RMS error  for fixed aperture mag.   \n')
+        f_1.write('# TTYPE8 = MAG / Best of MAG_AUTO and MAG_ISOCOR   \n')
+        f_1.write('# TTYPE9 = MAGERR / RMS error for MAG_BEST     \n')
+        f_1.write('# TTYPE10 = FLUX_MAX / Peak flux above background    \n')
+        f_1.write('# TTYPE11 = ISOAREA / Isophotal area above Analysis threshold    \n')
+        f_1.write('# TTYPE12 = X / Object position along x   \n')
+        f_1.write('# TTYPE13 = Y / Object position along y    \n')
+        f_1.write('# TTYPE14 = ra / Right ascension of barycenter (J2000)  \n')
+        f_1.write('# TTYPE15 = dec / Declination of barycenter (J2000)   \n')
+        f_1.write('# TTYPE16 = ixx / Variance along x     \n')
+        f_1.write('# TTYPE17 = iyy / Variance along y    \n')
+        f_1.write('# TTYPE18 = ixy / Covariance between x and y   \n')
+        f_1.write('# TTYPE19 = ixxWIN / Windowed variance along x    \n')
+        f_1.write('# TTYPE20 = iyyWIN / Windowed variance along y    \n')
+        f_1.write('# TTYPE21 = ixyWIN / Windowed covariance between x and y    \n')
+        f_1.write('# TTYPE22 = A / Profile RMS along major axis    \n')
+        f_1.write('# TTYPE23 = B / Profile RMS along minor axis    \n')
+        f_1.write('# TTYPE24 = THETA / Position angle (CCW/x)    \n')
+        f_1.write('# TTYPE25 = ELONGATION / A_IMAGE/B_IMAGE    \n')
+        f_1.write('# TTYPE26 = ELLIPTICITY / 1 - B_IMAGE/A_IMAGE    \n')
+        f_1.write('# TTYPE27 = FWHM / FWHM assuming a gaussian core    \n')
+        f_1.write('# TTYPE28 = FLAGS / Extraction flags    \n')
+        f_1.write('# TTYPE29 = CLASS_STAR / S/G classifier output     \n')
+
+        cont = 1
+        number_of_lost_1 = 0
+        number_of_lost_2 = 0
+        
+        #define array of catalog names
+        
+        names = ["number", "flux_iso", "fluxerr_iso", "mag_iso", "magger_iso", "mag_aper_1", "magerr_aper_1", "mag", "magger", "flux_max", "isoarea", "x", "y", "ra", "dec", "ixx", "iyy", "ixy", "ixxWIN", "iyyWIN", "ixyWIN", "A", "B", "theta", "enlogation", "ellipticity", "FWHM", "flags", "class_star"]
+
+        if compare == '1to2':
+
+            for index in self.posible_obj_index_1:
+                if index < len(self.positions_cat_1):
+                
+                    f_1.write('%-20s\t%-20s\t%-20s\t%-20s\t%-20s\t%-20s\t%-20s\t%-20s\t%-20s\t%-20s\t%-20s\t%-20s\t%-20s\t%-20s\t%-20s\t%-20s\t%-20s\t%-20s\t%-20s\t%-20s\t%-20s\t%-20s\t%-20s\t%-20s\t%-20s\t%-20s\t%-20s\t%-20s\t%-20s \n'% (cont, self.catalog_1[names[1]], self.catalog_1[names[2]], self.catalog_1[names[3]], self.catalog_1[names[4]], self.catalog_1[names[5]], self.catalog_1[names[6]], self.catalog_1[names[7]], self.catalog_1[names[8]], self.catalog_1[names[9]], self.catalog_1[names[10]], self.catalog_1[names[11]], self.catalog_1[names[12]], self.catalog_1[names[13]], self.catalog_1[names[14]], self.catalog_1[names[15]], self.catalog_1[names[16]], self.catalog_1[names[17]], self.catalog_1[names[18]], self.catalog_1[names[19]], self.catalog_1[names[20]], self.catalog_1[names[21]], self.catalog_1[names[22]], self.catalog_1[names[23]], self.catalog_1[names[24]], self.catalog_1[names[25]], self.catalog_1[names[26]], self.catalog_1[names[27]], self.catalog_1[names[28]]))
+                
+                    cont = cont + 1
+
+                elif index == len(position_all):
+                    number_of_lost_1 = number_of_lost + 1
     
-        f_1.write('%-20s\t%-20s\t%-20s\t%-20s\t%-20s \n'% (fit_1_int.c0_0.value, fit_1_int.c0_1.value, fit_1_int.c0_2.value, fit_1_int.c0_3.value, fit_1_int.c0_4.value))
+        print 'The number of lost objects from matching of 1 with respect of 2 is {}\n'.format(number_of_lost_1)
+        print 'Length of the Picture 1 original catalog {}\n'.format(len(self.catalog_1[names[0]]))
+        print 'Length of the Cross-Matching Catalog {}\n'.format(cont)
 
 
+        if compare == '1to2':
 
+            for index in self.posible_obj_index_2:
+                if index < len(self.positions_cat_2):
+                    
+                    f_1.write('%-20s\t%-20s\t%-20s\t%-20s\t%-20s\t%-20s\t%-20s\t%-20s\t%-20s\t%-20s\t%-20s\t%-20s\t%-20s\t%-20s\t%-20s\t%-20s\t%-20s\t%-20s\t%-20s\t%-20s\t%-20s\t%-20s\t%-20s\t%-20s\t%-20s\t%-20s\t%-20s\t%-20s\t%-20s \n'% (cont, self.catalog_2[names[1]], self.catalog_2[names[2]], self.catalog_2[names[3]], self.catalog_2[names[4]], self.catalog_2[names[5]], self.catalog_2[names[6]], self.catalog_2[names[7]], self.catalog_2[names[8]], self.catalog_2[names[9]], self.catalog_2[names[10]], self.catalog_2[names[11]], self.catalog_2[names[12]], self.catalog_2[names[13]], self.catalog_2[names[14]], self.catalog_2[names[15]], self.catalog_2[names[16]], self.catalog_2[names[17]], self.catalog_2[names[18]], self.catalog_2[names[19]], self.catalog_2[names[20]], self.catalog_2[names[21]], self.catalog_2[names[22]], self.catalog_2[names[23]], self.catalog_2[names[24]], self.catalog_2[names[25]], self.catalog_2[names[26]], self.catalog_2[names[27]], self.catalog_2[names[28]]))
+                        
+                    cont = cont + 1
+                            
+                elif index == len(position_all):
+                    number_of_lost_2 = number_of_lost_2 + 1
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+        print 'The number of lost objects from matching of 2 with respect of 1 is {}\n'.format(number_of_lost_2)
+        print 'Length of the Picture 2 original catalog {}\n'.format(len(self.catalog_2[names[0]]))
+        print 'Length of the Cross-Matching Catalog {}\n'.format(cont)
 
---
+++ WL_CrossMatching.py
@@ -0,0 +1,50 @@
+# Name: Class_CrossMatching.py
+#
+# Bachelor Disertation Program X
+#
+# Type: python class
+#
+# Content: 2 Classes, 1 constructor,
+#
+# Description: General Class destinated to get a cross matching between two catalogs from different filtered pics
+
+
+__author__ = "Guadalupe Canas Herrera"
+__copyright__ = "Copyright (C) 2015 G. Canas Herrera"
+__license__ = "Public Domain GNU"
+__version__ = "1.0.0"
+__maintainer__ = "Guadalupe Canas Herrera"
+__email__ = "gch24@alumnos.unican.es"
+
+
+import matplotlib.pyplot as plt #Plot Libraries
+import numpy as np #Maths arrays and more, matlab-type vectors/arrays
+import sys #Strings inputs
+import math #mathematical functions
+import subprocess #calling to the terminal
+from astropy.io import fits #Open and Reading FITS Files usign astropy
+import random #pseudo-random generator
+import seaborn as sns #Improvements for statistical-plots
+from WL_Utils import sex_caller
+from Class_CrossMatching import CrossMatching
+
+
+#Get first catalogs
+sex_caller('lhn1n1_2010apr_r_stack_fc_fix.fits'.format(mag), 'lhn1n1_2010apr_r_stack_fc_fix')
+sex_caller('lhn1n1_2010dec_z_stack_fc_fix.fits'.format(mag), 'lhn1n1_2010dec_z_stack_fc_fix')
+
+#Read catalogs
+catag_r = CatalogReader('lhn1n1_2010apr_r_stack_fc_fix.fcat')
+catag_r.read()
+
+catag_z = CatalogReader('lhn1n1_2010dec_z_stack_fc_fix.fcat')
+catag_z.read()
+
+#Create object for cross-matching
+
+crossmatching = CrossMatching(catag_r.fcat, catag_z.fcat)
+crossmatching.kdtree(n=2)
+crossmatching.catalog_writter(self, 'lhn1n1_crossmatching_1to2', compare = '1to2'):
+
+
+
