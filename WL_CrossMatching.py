2471af51d14e8bf66d7057de5e0d70addf4adf04
gcanasherrera <guadalupecah@gmail.com>
2015-11-04 17:29:32 +0100

    try

--- WL_CrossMatching.py
+++ WL_CrossMatching.py
@@ -44,7 +44,7 @@ catag_z.read()
 #Create object for cross-matching
 
 crossmatching = CrossMatching(catag_r.fcat, catag_z.fcat)
-crossmatching.kdtree(n=7.2exp-5)
+crossmatching.kdtree(n=7.2e-05)
 crossmatching.catalog_writter('lhn1n1_crossmatching_1to2', compare = '1to2')
 
 
