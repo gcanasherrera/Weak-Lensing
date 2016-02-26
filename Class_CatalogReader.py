d7683f97f0e7a63423f7f22ae89c3f47640acf29
gcanasherrera <guadalupecah@gmail.com>
2016-02-26 18:34:38 +0100

    attached final coments

--- Class_CatalogReader.py
+++ Class_CatalogReader.py
@@ -1,19 +1,20 @@
 # Name: ObjectCreator.py
 #
-# Bachelor Disertation Program VI
+# Bachelor Disertation Program A.I
 #
-# Type: Script
+# Type: Python Class
 #
-# Description:
+# Content: 1 class, 1 constructor, 2 methods
 #
-# Returns: FITS image including the objects we created
+# Description: Read Source Extractor and FIAT catalogues transforming information into structured arrays from numpy
+#
 #
 
 
 __author__ = "Guadalupe Canas Herrera"
 __copyright__ = "Copyright (C) 2015 G. Canas Herrera"
 __license__ = "Public Domain GNU"
-__version__ = "1.0.0"
+__version__ = "2.0.0"
 __maintainer__ = "Guadalupe Canas Herrera"
 __email__ = "gch24@alumnos.unican.es"
 
@@ -22,7 +23,17 @@ import numpy as np #Maths arrays and more, matlab-type vectors/arrays
 import subprocess #calling to the terminal
 import sys #Strings inputs
 
+"""
+    General Class that contents several methods destinated to read catalogues
+    
+"""
+
 class CatalogReader(object):
+    
+    """
+        Constructor that reads the name of the catalogue and split the name into the name itself and the ending
+        
+    """
     
     def __init__(self, catalog):
         
@@ -32,17 +43,28 @@ class CatalogReader(object):
         
         self.fcat =[]
         self.catalog_fiat=''
+    
+    """
+        Method that transform the Source Extractor catalogue into FIAT catalogue
+        
+    """
         
     def transform(self):
         self.catalog_fiat = 'fiat_{}.fcat'.format(self.FILE_NAME)
         transform_into_fiat='perl sex2fiat.pl {}>{}'.format(self.catalog, self.catalog_fiat)
         subprocess.call(transform_into_fiat, shell=True)
     
+    
+    """
+        Method that reads the catalogue and transforms it into numpy arrays
+        
+    """
+    
     def read(self):
         
         suffix = '.fcat'
-        
-        names = ["number", "flux_iso", "fluxerr_iso", "mag_iso", "magger_iso", "mag_aper_1", "magerr_aper_1", "mag", "magger", "flux_max", "isoarea", "x", "y", "ra", "dec", "ixx", "iyy", "ixy", "ixxWIN", "iyyWIN", "ixyWIN", "A", "B", "theta", "enlogation", "ellipticity", "FWHM", "flags", "class_star"]
+        names = ["x", "y", "mag_iso", "median", "ixx", "iyy", "ixy", "a_input", "b_input", "theta", "ellipticity", "errcode", "sigsky", "size", "flux", "mean_rho_4th", "sigma_e", "wander"]
+        #names = ["number", "flux_iso", "fluxerr_iso", "mag_iso", "magger_iso", "mag_aper_1", "magerr_aper_1", "mag", "magger", "flux_max", "isoarea", "x", "y", "ra", "dec", "ixx", "iyy", "ixy", "ixxWIN", "iyyWIN", "ixyWIN", "A", "B", "theta", "enlogation", "ellipticity", "FWHM", "flags", "class_star"]
         
         #For older version of Source Extractor
         
@@ -54,3 +76,5 @@ class CatalogReader(object):
 
         else:
             self.fcat = np.genfromtxt(self.catalog, names=names)
+
+
