02a06129e8b7ad64c5534df6fbcc2e33a115955f
gcanasherrera <guadalupecah@gmail.com>
2016-02-27 14:07:54 +0100

    attached final coments

--- Plotter_ObjectCreator.py
+++ Plotter_ObjectCreator.py
@@ -1,3 +1,12 @@
+# Name: Plotter_ObjectCreator.py
+#
+# Weak-Lensing Validation Program V
+#
+# Type: Python script
+#
+# Description: Plots feautres associated to the text files saved by WL_Simulation_execute_mag.py
+#
+
 
 from Class_ObjectCreator import ObjectCreator, MagnitudeExponential
 from Class_CatalogReader import CatalogReader
@@ -7,6 +16,10 @@ import matplotlib.pyplot as plt #Plot Libraries
 import seaborn as sns #Improvements for statistical-plots
 from operator import truediv
 import math
+
+import matplotlib 
+matplotlib.rc('xtick', labelsize=60) 
+matplotlib.rc('ytick', labelsize=60) 
 
 FILTER = 'MH'
 PICTURE = 'lhn1n1_2010apr_r_stack_fc_fix'
@@ -43,9 +56,16 @@ mag_input_iso = []
 sns.set(style="white", palette="muted", color_codes=True)
 plt.figure()
 #plt.title('Number of lost Galaxies vs mag_input_max')
-plt.plot(mag_input, number_lost_objects, 'k-')
-plt.xlabel('$m(max)_{input}$', fontsize=18)
-plt.ylabel('$n_{lost}$', fontsize=18)
+
+number_lost_objects_per = number_lost_objects/np.amax(number_lost_objects)*100
+plt.plot(mag_input, number_lost_objects_per, 'k-')
+
+plt.xticks(color='k', size=26)
+plt.yticks(color='k', size=26)
+
+
+plt.xlabel('$m_{max}^{i}$', fontsize=44)
+plt.ylabel('$n_{lost}/\%$', fontsize=44)
 plt.show()
 
 
@@ -59,8 +79,8 @@ plt.figure()
 plt.errorbar(flux_input, flux_output_max, flux_output_max_error, 0, fmt='ko')
 #plt.xlim(9, 1e12)
 #plt.ylim(9, 1e12)
-plt.xlabel('$F(max)_{input}$', fontsize=18)
-plt.ylabel('$F(max)_{output}$', fontsize=18)
+plt.xlabel('$F(max)_{input}$', fontsize=24)
+plt.ylabel('$F(max)_{output}$', fontsize=24)
 plt.show()
 
 #PLOT 3: flux_out_iso vs flux_input_iso
@@ -82,8 +102,8 @@ plt.figure()
 plt.errorbar(flux_input_iso, flux_output, flux_output_error, 0, fmt='ko')
 #plt.xlim(0.1, 1e12)
 #plt.ylim(0.1, 1e12)
-plt.xlabel('$F(iso)_{input}$', fontsize=18)
-plt.ylabel('$F(iso)_{output}$', fontsize=18)
+plt.xlabel('$F(iso)_{input}$', fontsize=24)
+plt.ylabel('$F(iso)_{output}$', fontsize=24)
 plt.show()
 
 
@@ -98,9 +118,30 @@ sns.set(style="white", palette="muted", color_codes=True)
 plt.figure()
 #plt.title('mag_out_iso vs mag_input_iso (Linear Scale)')
 plt.errorbar(mag_input_iso, mag_output_sex, mag_output_error_sex, 0, fmt='ko')
-plt.xlabel('$m(iso)_{input}$', fontsize=18)
-plt.ylabel('$m(iso)_{output}$', fontsize=18)
+plt.xlabel('$m_{iso}^{i}$', fontsize=44)
+plt.ylabel('$m_{iso}^{o}$', fontsize=44)
+plt.xticks(color='k', size=23)
+plt.yticks(color='k', size=23)
+plt.xlim(0,30)
+plt.ylim(0,25)
 plt.show()
+
+#PLOT 5: Number of lost Galaxies vs mag_input_iso
+sns.set(style="white", palette="muted", color_codes=True)
+plt.figure()
+#plt.title('Number of lost Galaxies vs mag_input_max')
+number_lost_objects_per = number_lost_objects/np.amax(number_lost_objects)*100
+plt.semilogy(mag_input_iso, number_lost_objects_per, 'k-')
+plt.xticks(color='k', size=23)
+plt.yticks(color='k', size=23)
+plt.xlabel('$m_{iso}^{i}$', labelpad=10, fontsize=40)
+plt.ylabel('$n_{lost}/\%$', fontsize=44)
+#plt.xlim(0,30)
+plt.ylim(0,110)
+plt.show()
+
+
+
 
 
 
